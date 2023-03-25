4aa5acf5258e736e906132a8487da8a02711c714
iceki7 <1300953421@qq.com>
2023-03-25 17:25:14 +0800

    info entropy

---
+++ 杂项/easyPy.py
@@ -0,0 +1,23 @@
+import os
+
+import sys
+
+print(sys.version)#E
+print(sys.executable)#E
+print(os.getcwd())
+#print(tf.test.is_gpu_available())
+
+# np.array
+# .shape
+# .ravel
+# .reshape(ar,[W,L])
+# .meshgrid
+# k=k[k==k]
+# plt.xlim
+# ax3.set_facecolor('#000000') 
+# ax3.plot_surface(X, Y, ar, cmap='viridis')  # 模糊一点
+# plt.gca().view_init(30, -30)    #默认是30和-60
+# plt.gca().dist=7 #默认是10
+# plt.axis('off')
+# plt.show()
+# fig.savefig(output_path,dpi=800,bbox_inches='tight')
---
+++ 杂项/easySh.sh
@@ -0,0 +1,15 @@
+cat file
+echo $XXX
+cp oldfile newfile
+mv file dir
+--xxx=
+netstat -tunlp|grep PORT
+netstat -anp | grep 15148 得到PID
+ps -aux | grep jupyter
+kill -9 pid
+touch 
+which
+
+cmd &
+# comment
+>> xx.log
